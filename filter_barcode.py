0194ccf8747c18fd5661441271335477a5122a79
Estolol <edouard.dufour-sans@polytechnique.edu>
2017-02-05 12:50:18 +0100

    changed usage text

--- filter_barcode.py
+++ filter_barcode.py
@@ -3,14 +3,14 @@ import sys
 if __name__ == '__main__':
 
     if(len(sys.argv)<3):
-        print("Usage:\npython3 filter_barcode.py <file_base> <min>.\nFor instance to remove every bar of length less than 0.5 from filtration B: \"python3 filter_barcode.py filtration_B 0.5\"")
+        print("Usage:\npython3 filter_barcode.py <file_base> <min>.\nFor instance to remove every bar of length less than 0.05 from filtration B: \"python3 filter_barcode.py filtration_B 0.05\"")
         quit()
     base = sys.argv[1]
     f = open("results/"+base+".out",'r')
     try:
         mini = float(sys.argv[2])
     except ValueError:
-        print("Usage:\npython3 filter_barcode.py <file> <min>.\nFor instance to remove every bar of length less than 0.5 from filtration B: \"python3 filter_barcode.py results/filtration_B.out 0.5\"")
+        print("Usage:\npython3 filter_barcode.py <file_base> <min>.\nFor instance to remove every bar of length less than 0.05 from filtration B: \"python3 filter_barcode.py filtration_B 0.05\"")
         quit()
     out = open("results/"+base+"_filtered.out",'w')
     for line in f:
